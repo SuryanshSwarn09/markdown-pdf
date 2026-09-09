c67d8eae20d2460fe6eccaece021a55facd1729a
SURYANSHSwarn09 <suryanshswarn@gmail.com>
2026-09-09 23:51:20 +0530

    feat(highlight): register core web languages (javascript, typescript, xml, css, json)

--- src/utils/highlighter.js
+++ src/utils/highlighter.js
@@ -1,5 +1,21 @@
 import hljs from 'highlight.js/lib/core';
 
+// 1. Core Web Languages
+import javascript from 'highlight.js/lib/languages/javascript';
+import typescript from 'highlight.js/lib/languages/typescript';
+import xml from 'highlight.js/lib/languages/xml';
+import css from 'highlight.js/lib/languages/css';
+import json from 'highlight.js/lib/languages/json';
+
+hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('js', javascript);
+hljs.registerLanguage('typescript', typescript);
+hljs.registerLanguage('ts', typescript);
+hljs.registerLanguage('xml', xml);
+hljs.registerLanguage('html', xml);
+hljs.registerLanguage('css', css);
+hljs.registerLanguage('json', json);
+
 /**
  * Highlights a block of code using registered languages with a safe fallback to plaintext.
  *
